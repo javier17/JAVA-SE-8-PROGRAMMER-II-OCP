c58ccedfe25d6987192c7e618e7687e5e7934e0a
Carlos Javier Pita Amaya <javierpita17@hotmail.com>
2021-02-18 19:33:10 -0500

    Implementar poliformismo y conocer sus beneficios

--- Chapter02Code/src/main/java/mx/com/hittechs/animals/test/AnimalsTest.java
+++ Chapter02Code/src/main/java/mx/com/hittechs/animals/test/AnimalsTest.java
@@ -1,12 +1,37 @@
 package mx.com.hittechs.animals.test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import mx.com.hittechs.animals.Animal;
+import mx.com.hittechs.animals.Cat;
+import mx.com.hittechs.animals.Dog;
+
 /**
  * @author Hittechs Training & Consulting
  * @web www.hittechs.com.mx
  * @mail contacto@hittechs.com.mx
  */
 public class AnimalsTest {
+     
+    public static void feed(Animal a){
+        a.eat();
+    }
+    
     public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
         
+        //feed(cat);
+        //feed(dog);
+        
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Cat());
+        animals.add(new Dog());
+        
+        for (Animal animal: animals) {
+          animal.eat();
+        }
     }
 }
